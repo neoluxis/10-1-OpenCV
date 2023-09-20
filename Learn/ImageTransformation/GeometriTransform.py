7d59feda801254aba152248f30bcf0931b3d1ba1
Neolux Lee <neolux.lee@gmail.com>
2023-09-20 20:04:20 +0800

    pull images

--- Learn/ImageTransformation/GeometriTransform.py
+++ Learn/ImageTransformation/GeometriTransform.py
@@ -1,7 +1,16 @@
 import cv2 as cv
 
 if __name__ == "__main__":
-    cam = cv.VideoCapture(0)
+    cam = cv.VideoCapture(2)
+    cam.set(3, 640)
+    cam.set(4, 320)
     while True:
-        ret, frame = cam.read()
-        cv.imshow('frame', frame)
+        _, img = cam.read()
+        img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        # _, img = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
+        img = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 15, 10)
+        cv.imshow("Original", img)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+    cam.release()
+    cv.destroyAllWindows()
