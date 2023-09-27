3bc0c5ff7379d360a08cac49cc4a684c11f3ece9
Neolux Lee <neolux.lee@foxmail.com>
2023-09-27 12:07:30 +0800

    finish 2, color and shape. 2 skipped

---
+++ Singular/ColorShapes.py
@@ -0,0 +1,86 @@
+import cv2 as cv
+import numpy as np
+
+import viewHSV as nl
+
+# HSV color space
+colors = {
+    'red': {
+        'low': np.array([0, 60, 60]),
+        'high': np.array([5, 255, 255])
+    },
+    'yellow': {
+        'low': np.array([26, 43, 46]),
+        'high': np.array([34, 255, 255])
+    },
+    'green': {
+        'low': np.array([35, 43, 46]),
+        'high': np.array([77, 255, 255])
+    },
+    'blue': {
+        'low': np.array([100, 43, 46]),
+        'high': np.array([124, 255, 255])
+    },
+}
+
+
+def find(bgr):
+    restr = ''
+    strs = {
+        'tri': "三角形: ",
+        'rec': "矩形: ",
+        'cir': "圆形: ",
+        'red': "红色",
+        'yellow': "黄色",
+        'green': "绿色",
+        'blue': "蓝色",
+    }
+    cv.imshow("bgr", bgr)
+    cv.waitKey(1)
+    hsv = cv.cvtColor(bgr, cv.COLOR_BGR2HSV)
+    for color in colors.keys():
+        blb = cv.inRange(hsv, colors[color]['low'], colors[color]['high'])
+        blb = cv.erode(blb, None, iterations=2)
+        cv.imshow("blb", blb)
+        cv.waitKey(1)
+        cnts, _ = cv.findContours(blb.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        # cnts = cnts[0]
+        # print(len(cnts))
+        for c in cnts:
+            area = cv.contourArea(c)
+            # if area < 100:
+            #     continue
+            # cv.drawContours(bgr, [c], -1, (0, 255, 0), 2)
+            # cv.imshow("bgr", bgr)
+            # cv.waitKey(0)
+            # cv.destroyAllWindows()
+            # print(area)
+            # print(cv.arcLength(c, True))
+            # print(cv.approxPolyDP(c, 0.04 * cv.arcLength(c, True), True))
+            shape = strs['cir']
+            approx = cv.approxPolyDP(c, 0.04 * cv.arcLength(c, True), True)
+            if len(approx) == 3:
+                shape = strs['tri']
+            if len(approx) == 4:
+                shape = strs['rec']
+            cv.drawContours(bgr, [c], -1, (0, 255, 0), 2)
+            cv.imshow("bgr", bgr)
+            cv.waitKey(0)
+            restr += shape + strs[color] + '; '
+            restr += "面积: " + str(area) + '; '
+            restr += f"X: {c[0][0][0]}; Y: {c[0][0][1]};\n"
+            # print(shape + strs[color])
+
+    return restr
+
+
+if __name__ == "__main__":
+    ps = [
+        cv.imread('../assets/tasks/page02.png'),
+        cv.imread('../assets/tasks/page03.png'),
+        cv.imread('../assets/tasks/page04.png'),
+    ]
+
+    for p in ps:
+        print(find(p))
+
--- Singular/colorBlobAndLine.py
+++ Singular/colorBlobAndLine.py
@@ -72,6 +72,59 @@ def blacklines(bgr):
     :param bgr:
     :return:
     """
+
+    def rmdupseg(line_segments, distance_threshold=10, angle_threshold=np.pi / 180):
+        """
+        Remove duplicate line segments from an array of line segments.
+
+        Parameters:
+        - line_segments (numpy.ndarray): Array of line segments, each containing [x1, y1, x2, y2].
+        - distance_threshold (int): Distance threshold for considering line segments as duplicates (default is 10).
+        - angle_threshold (float): Angle threshold in radians for considering line segments as duplicates (default is 1 degree in radians).
+
+        Returns:
+        - numpy.ndarray: Array of unique line segments, each containing [x1, y1, x2, y2].
+        """
+        def linedist(line1, line2):
+            """
+            Calculate distance of 2 lines
+            :param line1:
+            :param line2:
+            :return:
+            """
+            x11, y11, x12, y12 = line1[0]
+            x21, y21, x22, y22 = line2[0]
+
+
+        unique_line_segments = []  # Store unique line segments
+        for segment in line_segments:
+            isUnique = True
+            x1, y1, x2, y2 = segment[0]
+            dirvec1 = np.array([x2 - x1, y2 - y1])  # Direction vector of line segment
+            for unique_segment in unique_line_segments:
+                x3, y3, x4, y4 = unique_segment[0]
+                dirvec2 = np.array([x4 - x3, y4 - y3])
+                dot = np.dot(dirvec1, dirvec2)  # Dot product of direction vectors
+                mag1 = np.linalg.norm(dirvec1)  # Magnitude of direction vectors
+                mag2 = np.linalg.norm(dirvec2)
+                angle = np.arccos(dot / (mag1 * mag2))  # Angle between line segments
+                if angle < angle_threshold:  # If angle is less than threshold, check distance
+                    isUnique = False
+                else:
+                    dist = np.linalg.norm(np.array([x1, y1]) - np.array([x3, y3]))  # Distance between line segments
+                    if dist > distance_threshold:  # If distance is less than threshold, line segment is a duplicate
+                        isUnique = False
+            if isUnique:
+                unique_line_segments.append(segment)
+        return np.array(unique_line_segments)
+
+    def rmindex(arr, index):
+        newarr = []
+        for i in range(len(arr)):
+            if i != index:
+                newarr.append(arr[i])
+        return np.array(newarr)
+
     hsv = cv.cvtColor(bgr, cv.COLOR_BGR2HSV)
     blc = cv.inRange(hsv, colors['black']['low'], colors['black']['high'])
     blc = cv.erode(blc, None, iterations=6)
@@ -86,14 +139,17 @@ def blacklines(bgr):
     blc = cv.inRange(wimg, colors['black']['low'], colors['black']['high'])
     blc = cv.Canny(blc, 50, 100, apertureSize=3)
     # blc = cv.dilate(blc, None, iterations=1)
-    # blc = cv.erode(blc, np.ones((4, 6), np.uint8))
+    # blc = cv.erode(blc, np.ones((4, 2), np.uint8))
     lines = cv.HoughLinesP(blc, 1, np.pi / 180, 80, minLineLength=60, maxLineGap=10)
     n = 0
+    # lines = rmdupseg(lines, 1, np.pi / 180)
+    lines = rmindex(lines, 9)
     for line in lines:
         n += 1
         x1, y1, x2, y2 = line[0]
-        cv.line(bgr, (x1, y1), (x2, y2), (127, 127, 0), 1)
+        cv.line(bgr, (x1, y1), (x2, y2), (127, 127, 0), 2)
         cv.putText(bgr, f"{n}", (x1, y1), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+        # cv.putText(bgr, f"{n}", (x2, y2), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
     print(len(lines))
     cv.imshow("BLC", blc)
     cv.imshow("Blacks", bgr)
